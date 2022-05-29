84ae42497cc0c572ec36ad2e2224f7f97408be86
ixwrites <74097341+ixwrites@users.noreply.github.com>
2022-05-29 12:19:13 +0800

    fx

--- guessing-game.js
+++ guessing-game.js
@@ -14,12 +14,22 @@ let randomNumber = 0;
 let guesses = 0;
 let gameOngoing = false;
 
+//CASH STAT
+if (localStorage.getItem('total_game_cash') === null) {
+  localStorage.setItem('total_game_cash', 0);
+  gameCashUI.innerHTML = `CASH: $0`
+} else {
+  setInterval(updateGameCashTicker, 33);
+  function updateGameCashTicker() {
+    gameCashUI.innerHTML = `CASH: $${localStorage.getItem('total_game_cash').toLocaleString()}`
+  }
+}
+
 // WINS STAT
 if (localStorage.getItem('total_game_wins') === null) {
   localStorage.setItem('total_game_wins', 0);
-  totalWinsUI.innerHTML = `Wins: 0`
-}
-else {
+  totalWinsUI.innerHTML = `WINS: 0`
+} else {
   setInterval(updateGameWinsTicker, 33);
   function updateGameWinsTicker() {
     totalWinsUI.innerHTML = `WINS: ${localStorage.getItem('total_game_wins').toLocaleString()}`
@@ -29,24 +39,11 @@ else {
 // LOSSES STAT
 if (localStorage.getItem('total_game_losses') === null) {
   localStorage.setItem('total_game_losses', 0);
-  totalLossesUI.innerHTML = `Losses: 0`
-}
-else {
+  totalLossesUI.innerHTML = `LOSSES: 0`
+} else {
   setInterval(updateGameLossesTicker, 33);
   function updateGameLossesTicker() {
     totalLossesUI.innerHTML = `LOSSES: ${localStorage.getItem('total_game_losses').toLocaleString()}`
-  }
-}
-
-//CASH STAT
-if (localStorage.getItem('total_game_cash') === null) {
-  localStorage.setItem('total_game_cash', 0);
-  gameCashUI.innerHTML = `Cash: $0`
-}
-else {
-  setInterval(updateGameCashTicker, 33);
-  function updateGameCashTicker() {
-    gameCashUI.innerHTML = `CASH: $${localStorage.getItem('total_game_cash').toLocaleString()}`
   }
 }
 
@@ -68,50 +65,51 @@ $(document).on('keydown', function (e) {
   }
 });
 
+function incrementGameWins() {
+  localStorage.setItem('total_game_wins', localWins += 1);
+}
+
+function decrementGuessesTicker() {
+  guessesUI.innerHTML = `Remaining Guesses: ${guesses}`
+}
+
 /**************************************************************************************/
 
 // Start the game
 function startGuessingGame() {
   generateRandomNumber();
   gameOngoing = true;
-
   $('#start-btn').fadeOut();
   setTimeout(function () {
     $('#guessing-game-input').fadeIn();
+    setInterval(decrementGuessesTicker, 33);
     gameInput.focus();
     // document.querySelector('#debug-answer').innerHTML = `Debug Answer: ${randomNumber}`;
-    guessesUI.innerHTML = `Remaining Guesses: 5`;
+    guesses = 5;
     systemMessageUI.innerHTML = ``
   }, 500);
 
   gameInput.addEventListener('keypress', function (e) {
     if (event.key === 'Enter' && gameInput.value.length > 0 && gameInput.value <= 100 && guesses > 0) {
 
-      guessesUI.innerHTML = `Remaining Guesses: ${guesses}`;
-
       if (gameInput.value - randomNumber >= 30) {
         systemMessageUI.innerHTML = `[${gameInput.value.padStart(3, '0')}] Your guess is too high! <br> ${systemMessageUI.innerHTML}`;
-        guessesUI.innerHTML = `Remaining Guesses: ${guesses -= 1}`;
-      }
-      else if (gameInput.value - randomNumber >= 16) {
+        guesses -= 1;
+      } else if (gameInput.value - randomNumber >= 16) {
         systemMessageUI.innerHTML = `[${gameInput.value.padStart(3, '0')}] Your guess is high. <br> ${systemMessageUI.innerHTML}`;
-        guessesUI.innerHTML = `Remaining Guesses: ${guesses -= 1}`;
-      }
-      else if (gameInput.value - randomNumber >= 1 && gameInput.value - randomNumber < 16) {
+        guesses -= 1;
+      } else if (gameInput.value - randomNumber >= 1 && gameInput.value - randomNumber < 16) {
         systemMessageUI.innerHTML = `[${gameInput.value.padStart(3, '0')}] Your guess is a bit higher. <br> ${systemMessageUI.innerHTML}`;
-        guessesUI.innerHTML = `Remaining Guesses: ${guesses -= 1}`;
-      }
-      else if (gameInput.value - randomNumber <= -30) {
+        guesses -= 1;
+      } else if (gameInput.value - randomNumber <= -30) {
         systemMessageUI.innerHTML = `[${gameInput.value.padStart(3, '0')}] Your guess is too low! <br> ${systemMessageUI.innerHTML}`;
-        guessesUI.innerHTML = `Remaining Guesses: ${guesses -= 1}`;
-      }
-      else if (gameInput.value - randomNumber <= -16) {
+        guesses -= 1;
+      } else if (gameInput.value - randomNumber <= -16) {
         systemMessageUI.innerHTML = `[${gameInput.value.padStart(3, '0')}] Your guess is low. <br> ${systemMessageUI.innerHTML}`;
-        guessesUI.innerHTML = `Remaining Guesses: ${guesses -= 1}`;
-      }
-      else if (gameInput.value - randomNumber <= -1 && gameInput.value - randomNumber < 15) {
+        guesses -= 1;
+      } else if (gameInput.value - randomNumber <= -1 && gameInput.value - randomNumber < 15) {
         systemMessageUI.innerHTML = `[${gameInput.value.padStart(3, '0')}] Your guess is a bit lower. <br> ${systemMessageUI.innerHTML}`;
-        guessesUI.innerHTML = `Remaining Guesses: ${guesses -= 1}`;
+        guesses -= 1;
       }
 
       // IF Won
@@ -120,30 +118,29 @@ function startGuessingGame() {
         setTimeout(function () {
           $('#restart-btn').fadeIn();
         }, 500);
-
-        guessesUI.innerHTML = `Remaining Guesses: ${guesses -= 1}`;
+        guesses -= 1;
         gameOngoing = false;
 
         if (guesses == 4) {
-          localStorage.setItem('total_game_cash', localCash += 1 + localWins);
-          systemMessageUI.innerHTML = `You earned $${localWins + 1}. <br> [${gameInput.value.padStart(3, '0')}] You won in first try! Unbelievable! 😱 <br> ${systemMessageUI.innerHTML}`
-          localStorage.setItem('total_game_wins', localWins += 1);
+          localStorage.setItem('total_game_cash', localCash += (localWins + 1) * 5);
+          systemMessageUI.innerHTML = `You earned $${(localWins + 1) * 5}. <br> [${gameInput.value.padStart(3, '0')}] You won in first try! Unbelievable! 😱 <br> ${systemMessageUI.innerHTML}`
+          incrementGameWins();
         } else if (guesses == 3) {
-          localStorage.setItem('total_game_cash', localCash += 1 + localWins)
-          systemMessageUI.innerHTML = `You earned $${localWins + 1}. <br> [${gameInput.value.padStart(3, '0')}] You won in second try! Excellent! 🤩 <br> ${systemMessageUI.innerHTML}`
-          localStorage.setItem('total_game_wins', localWins += 1);
+          localStorage.setItem('total_game_cash', localCash += (localWins + 1) * 4);
+          systemMessageUI.innerHTML = `You earned $${(localWins + 1) * 4}. <br> [${gameInput.value.padStart(3, '0')}] You won in second try! Excellent! 🤩 <br> ${systemMessageUI.innerHTML}`
+          incrementGameWins();
         } else if (guesses == 2) {
-          localStorage.setItem('total_game_cash', localCash += 1 + localWins)
-          systemMessageUI.innerHTML = `You earned $${localWins + 1}. <br> [${gameInput.value.padStart(3, '0')}] You won in third try! Way to go! 🥳 <br> ${systemMessageUI.innerHTML}`
-          localStorage.setItem('total_game_wins', localWins += 1);
+          localStorage.setItem('total_game_cash', localCash += (localWins + 1) * 3);
+          systemMessageUI.innerHTML = `You earned $${(localWins + 1) * 3}. <br> [${gameInput.value.padStart(3, '0')}] You won in third try! Way to go! 🥳 <br> ${systemMessageUI.innerHTML}`
+          incrementGameWins();
         } else if (guesses == 1) {
-          localStorage.setItem('total_game_cash', localCash += 1 + localWins)
+          localStorage.setItem('total_game_cash', localCash += 1 + localWins);
           systemMessageUI.innerHTML = `You earned $${localWins + 1}. <br> [${gameInput.value.padStart(3, '0')}] You won! 🎁 Great job! <br> ${systemMessageUI.innerHTML}`
-          localStorage.setItem('total_game_wins', localWins += 1);
+          incrementGameWins();
         } else if (guesses == 0) {
-          localStorage.setItem('total_game_cash', localCash += 1 + localWins)
+          localStorage.setItem('total_game_cash', localCash += 1 + localWins);
           systemMessageUI.innerHTML = `You earned $${localWins + 1}. <br> [${gameInput.value.padStart(3, '0')}] You won! That's a close one! 😮‍💨 <br> ${systemMessageUI.innerHTML}`
-          localStorage.setItem('total_game_wins', localWins += 1);
+          incrementGameWins();
         }
       }
 
@@ -151,7 +148,6 @@ function startGuessingGame() {
       if (gameInput.value != randomNumber && guesses == 0) {
         $(gameInput).fadeOut();
         systemMessageUI.innerHTML = `Game over. 😭 <br><br> Secret Number: ${randomNumber} <br> ${systemMessageUI.innerHTML}`
-        guessesUI.innerHTML = `Remaining Guesses: ${guesses}`;
         gameOngoing = false;
         localStorage.setItem('total_game_losses', localLosses += 1);
         setTimeout(function () {
@@ -169,4 +165,13 @@ function startGuessingGame() {
     }
   });
 
+}
+
+function endGuessingGame() {
+  gameOngoing = false;
+  $('#guessing-game-input').fadeOut();
+  setTimeout(function () {
+    $('#start-btn').fadeIn();
+    systemMessageUI.innerHTML = ``
+  }, 500);
 }
