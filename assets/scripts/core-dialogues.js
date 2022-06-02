cd53cd87fcfcb0a35726b17c02b95efa096c1057
ixwrites <74097341+ixwrites@users.noreply.github.com>
2022-06-02 20:21:39 +0800

    fx

--- assets/scripts/core-dialogues.js
+++ assets/scripts/core-dialogues.js
@@ -51,8 +51,8 @@ borderPatrolDialogue2 =
     <p class="mid-em">Alright, <span class="blue-text">${citizenUsername}</span>, your profile looks good to me... Here's your papers. You're good to go. The exit is that way... Have a good day!</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('show_identification_2', true);gameStoryProgressTicker();fadeOut('#left-main-frame');">Okay</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('show_identification_2', true);gameStoryProgressTicker();fadeOut('#left-main-frame');">Thank you, officer</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('show_identification_2', true);gameStoryProgressTicker();fadeOut('#left-main-frame');">Okay</button>
 </div>`
 
 borderPatrolDialogue3 =
@@ -63,8 +63,8 @@ borderPatrolDialogue3 =
     <p class="mid-em">Hey, <span class="blue-text">${citizenUsername}</span>, ${citizenCallsign}, been waiting for you forever... and look at you, you still look ugly as hell!</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#border-patrol-dialogue-3');fadeInNormalDelay('#border-patrol-dialogue-4');">And you're not?</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#border-patrol-dialogue-3');fadeInNormalDelay('#border-patrol-dialogue-4');">I missed you too, ${siblingShort}</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');fadeOut('#border-patrol-dialogue-3');fadeInNormalDelay('#border-patrol-dialogue-4');">And you're not?</button>
 </div>
 
 <div class="collapse" id="border-patrol-dialogue-4">
@@ -74,8 +74,8 @@ borderPatrolDialogue3 =
     <p class="mid-em">Nah, I'm just kidding, I missed you, ${citizenCallsign}. It's been really tough here without family... <br><br> You can't trust anyone in this city. Everyone seems to be in a race, like rats...</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#border-patrol-dialogue-4');fadeInNormalDelay('#border-patrol-dialogue-5');">*sigh*</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#border-patrol-dialogue-4');fadeInNormalDelay('#border-patrol-dialogue-5');">Is it really that bad?</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');fadeOut('#border-patrol-dialogue-4');fadeInNormalDelay('#border-patrol-dialogue-5');">*sigh*</button>
 </div>
 
 <div class="collapse" id="border-patrol-dialogue-5">
@@ -86,8 +86,8 @@ borderPatrolDialogue3 =
     <p class="mid-em">You can sleep in the car while I drive, you must be tired...</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('family_reunited', true);gameStoryProgressTicker();fadeOut('#left-main-frame');">Okay, let's go.</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('family_reunited', true);gameStoryProgressTicker();fadeOut('#left-main-frame');">Thanks ${citizenCallsign}, really appreciate it.</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('family_reunited', true);gameStoryProgressTicker();fadeOut('#left-main-frame');">Okay, let's go.</button>
 </div>`
 
 razeBarPrologueTutorial =
@@ -109,8 +109,8 @@ razeBarPrologueTutorial =
     <p class="mid-em">Look, you see those gaming tables over there?</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-2');fadeInNormalDelay('#raze-bar-prologue-3');">Yeah...</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-2');fadeInNormalDelay('#raze-bar-prologue-3');">What's going on?</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-2');fadeInNormalDelay('#raze-bar-prologue-3');">Yeah...</button>
 </div>
 
 <div class="collapse" id="raze-bar-prologue-3">
@@ -120,8 +120,8 @@ razeBarPrologueTutorial =
     <p class="mid-em">That's the craze 'round here. A real money-making machine... that is if you're smart enough to crack it. And that's why you see a lot of people coming and going. <br><br> What d'you think?</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-3');fadeInNormalDelay('#raze-bar-prologue-4-b');">I don't really gamble, ${citizenCallsign}.</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-3');fadeInNormalDelay('#raze-bar-prologue-4-a');">How do I play it?</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-3');fadeInNormalDelay('#raze-bar-prologue-4-b');">I don't really gamble, ${citizenCallsign}.</button>
 </div>
 
 <div class="collapse" id="raze-bar-prologue-4-a">
@@ -131,8 +131,8 @@ razeBarPrologueTutorial =
     <p class="mid-em">Okay, so the mechanics is simple... you try to guess the <span class="blue-text">secret number</span> from <span class="blue-text">0</span> to <span class="blue-text">100</span> in just <span class="blue-text">5 tries</span>. If you can guess it correctly, you win, cash straight to your pocket.</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-4-a');fadeInNormalDelay('#raze-bar-prologue-5');">And if I lose?</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-4-a');fadeInNormalDelay('#raze-bar-prologue-5');">That sounds easy!</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-4-a');fadeInNormalDelay('#raze-bar-prologue-5');">And if I lose?</button>
 </div>
 
 <div class="collapse" id="raze-bar-prologue-4-b">
@@ -153,8 +153,8 @@ razeBarPrologueTutorial =
     <p class="mid-em">To play it, you have to pay a dollar. If you lose, dollar's gone, and if you win, you earn one back.</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-5');fadeInNormalDelay('#raze-bar-prologue-6');">Hmm...</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-5');fadeInNormalDelay('#raze-bar-prologue-6');">Doesn't sound easy money.</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-5');fadeInNormalDelay('#raze-bar-prologue-6');">Hmm...</button>
 </div>
 
 <div class="collapse" id="raze-bar-prologue-6">
@@ -174,8 +174,8 @@ razeBarPrologueTutorial =
     <p class="mid-em">Yup, to add to up to that, if you got it right in your first guess, your prize is <span class="blue-text">multiplied by 5</span>. If on second try, it's <span class="blue-text">multiplied by 4</span>, and if it's on the third try, it's <span class="blue-text">multiplied by 3</span>. That's it, no multiplier for the last 2 guesses, but remember, if you've won before, it will still add up to the total prize.</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-7');fadeInNormalDelay('#raze-bar-prologue-8');">I'm ready but...</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-7');fadeInNormalDelay('#raze-bar-prologue-8');">I think I get it now.</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');fadeOut('#raze-bar-prologue-7');fadeInNormalDelay('#raze-bar-prologue-8');">I'm ready but...</button>
 </div>
 
 <div class="collapse" id="raze-bar-prologue-8">
@@ -186,8 +186,8 @@ razeBarPrologueTutorial =
     <p class="mid-em">Just don't lose it all, alright?</p>
   </div>
 </div>
+<button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('raze_tutorial_complete', true);gameStoryProgressTicker();icreaseCitizenCash(20);fadeOut('#raze-prologue-tutorial');">You're the best, ${siblingShort}.</button>
 <button class="mt-4 dialogue-response-btn" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('raze_tutorial_complete', true);gameStoryProgressTicker();icreaseCitizenCash(20);fadeOut('#raze-prologue-tutorial');">I'll do my best.</button>
-<button class="mt-4 dialogue-response-btn me-3" onclick="playBackgroundMusic('click.wav');setProgressLocalStorage('raze_tutorial_complete', true);gameStoryProgressTicker();icreaseCitizenCash(20);fadeOut('#raze-prologue-tutorial');">You're the best, ${siblingShort}.</button>
 </div>
 </div>
 `
